295b045f390b05e60dcb8dd64fde1b96a5cdb6c9
Alan <alancym.wjr@gmail.com>
2023-03-01 16:10:15 +0800

    FlaskWeb 第二章HTTP

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, url_for, request,redirect,make_response # 从 flask 包导入 Flask类
+from flask import Flask, url_for, request,redirect,make_response,Markup # 从 flask 包导入 Flask类
 # 这个类表示一个Flask程序，实例化这个类，就得到我们的程序实例app
 # 传入Flask类构造方法的第一个参数是模块或者包的名称。
 # 使用特殊变量 __name__ Python会根据所处的模块来赋予 __name__ 变量相应的值
+from urllib.parse import urlparse,urljoin # URL 安全性处理
 app = Flask(__name__)
 
 # 注册路由
@@ -19,7 +20,12 @@ def hello_world():  # put application's code here
     # args返回字典键值对，如果直接使用request.args['name'],如果没有对应的名为name的键，则返回错误
     # 所以使用get()方法获取数据，如果没有对应值则返回None；get方法的第二个参数设置默认值
     return '<h2>Hello Flask!<h2>' \
-           f'<h2>request name is {name}<h2>'
+           f'<h2>request name is {Markup.escape(name)}<h2>' # 这种情况下再去执行下面的那条地址，则只会把name后面的东西原封不动的输出
+    # 即利用escape函数对用户传入的数据进行了转义，转义并不能避免所有xss攻击，还需要对用户输入数据进行类型验证（第四章）
+    # f'<h2>request name is {name}<h2>'
+    # 这种输出的话，如果输入'http://127.0.0.1:5000/hello?name=<script>Ealert('Bingo!');</script>'
+    # 则会执行alert，显示bingo的弹窗，也就意味着也可以执行其他JavaScript语句
+    # 为防范XSS攻击，对用户输入内容进行HTML转义 使用 Jinja2 提供的 escape 函数对用户传入的数据进行转义
 # 为函数附加 app.route()装饰器，并传入URL规则做为参数，使得URL与函数相关联
 # 此函数称为视图函数
 # 可以为URL中添加变量部分 <变量名>
@@ -56,5 +62,77 @@ def set_cookie(name):
     response.set_cookie('name',name)
     return response
 
+
+### HttpExercise
+@app.route('/foo')
+def foo():
+    url_string = url_for('do_something')
+    return f'<h1> Foo page </h1> <a href = {url_string} >  Do something </a>'
+#两个函数都指向 dosomething的视图链接
+@app.route('/bar')
+def bar():
+    url_string  = url_for('do_something')
+    return f'<h1> Bar page </h1> <a href = {url_string} >  Do something </a>'
+@app.route('/do_something')
+def do_something():
+    # do something
+    return redirect_back()
+
+def redirect_back(default = 'hello_world' , **kwargs):
+    for target in request.args.get('next'),request.referrer :
+        if not target:
+            continue
+        if is_safe_url(target):
+            # 检测是否为安全URL
+            return redirect(target)
+            # 返回上一个页面的地址（此地址存在于 request的args字典的next键的对应值中
+            # 或者 referrer字段中（防火墙软件或者浏览器可能会自动清除、修改referrer））
+    return redirect(url_for(default,**kwargs)) # 如果没有找到，默认返回hello_world视图函数界面
+# 但是 next参数 可以以查询字符串的方式写在URL里，所以任何人都可以发给某个用户一个包含next变量指向任何站点的链接
+# ‘http://127.0.0.1:5000/do_something?next=http://helloflask.com’将会返回helloflask网站
+#  所以要验证URL的安全性
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    ### 通过 host_url 获取程序内主机 URl
+    ### 接收 target 做目标 URL
+    test_url = urlparse(urljoin(request.host_url,target))
+    # 连接两个参数的url, 将第二个参数中缺的部分用第一个参数的补齐, 如果第二个有完整的路径，则以第二个为主。
+    return test_url.scheme in ('http','https') and ref_url.netloc == test_url.netloc
+    # urlparse 六个字段：其中 scheme 是协议  netloc 是域名服务器  path 相对路径  params是参数，query是查询的条件
+
+
+### 异步加载长文章示例
+from jinja2.utils import generate_lorem_ipsum
+@app.route('/post')
+def show_post():
+    post_body = generate_lorem_ipsum(n = 2) ## 生成两段随机文本（ n 参数指定段落数量，默认为 5 ）
+    return '''<h1>A very long post</h1>
+              <div class = "body"> %s </div>
+              <button id = 'load'> Load More </button>
+              <script src = 'https://code.jquery.com/jquery-3.3.1.min.js'></script>
+              <script type = 'text/javascript'>
+              $(function() {
+                $('#load').click(function() {
+                   $.ajax({
+                    url: '/more', //目标URL
+                    type: 'get', //请求方法
+                    success: function(data){ //返回2xx响应之后触发回调函数
+                       $('.body').append(data); //将返回的响应插入到页面中
+                    }
+                  })
+                })
+              })
+              </script>
+              ''' % post_body
+@app.route('/more')
+def load_post():
+    # 点击 load more 之后，在执行/more的视图函数，返回一段随机文本 插入到 post界面中
+    return generate_lorem_ipsum(n = 1)
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug = True)
+
