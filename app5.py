2b2b168afd28e088c99af392a74ee995a5fc489e
Alan <alancym.wjr@gmail.com>
2023-03-06 15:58:59 +0800

    Flask URL构建与简单重定向

--- app5.py
+++ app5.py
@@ -1,19 +1,25 @@
 
 from flask import Flask,render_template,request
 from werkzeug.wrappers.response import Response
-'''Flask 将表单数据发送到模板'''
+'''
+Flask 将表单数据发送到模板
+表单结构定义在HTML中
+'''
 app = Flask(__name__)
+
 
 @app.route('/')
 def student():
     return render_template('app5.html')
 
-@app.route('/result',methods = ['POST','GET'])
+
+@app.route('/result',methods=['POST', 'GET'])
 def result():
     if request.method == 'POST':
         rst = request.form
         # 取得表单数据
-        return render_template('result5.html',result = rst)
+        return render_template('result5.html', result=rst)
+
 
 if __name__ == '__main__':
     Flask.run(debug=True)
