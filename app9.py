4a1ab17099af644b5f844ffd3860439f0d190556
Alan <alancym.wjr@gmail.com>
2023-03-06 18:46:17 +0800

    Flask flash消息闪现演示实例

--- app9.py
+++ app9.py
@@ -1,24 +1,31 @@
 from flask import Flask,flash,redirect,\
     render_template,request,url_for
 '''Flask 消息闪现'''
+import os
+secretKey = os.urandom(32)
 app = Flask(__name__)
-app.secret_key='123456'
+app.secret_key = secretKey
+
 
 @app.route('/')
 def index():
     return render_template('app8.html')
 
-@app.route('/login',methods=['POST','GET'])
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    errormsg = None
+    errorMessage = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            errormsg = "Invalid username or password.Please try again!"
+            errorMessage = "username或password 不为 'admin' "
+            print(errorMessage)
         else:
-            flash('You are successfully logged in')
+            flash("用户名与密码都为 'admin' ,登录成功 You are successfully logged in")
             return redirect(url_for('index'))
-    # GET 或者 POST 中username与password不全为admin
-    return render_template('login8.html',error = errormsg)
+
+    return render_template('login8.html', error=errorMessage)
+    # GET 或者 POST 中username与password不全为admin的时候，返回login8.html,给error变量赋值errorMessage
+
 
 if __name__ == '__main__':
     Flask.run(debug=True)
