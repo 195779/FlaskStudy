c770ab26118cf102b06e20565c1e87380d4e7cc9
Alan <alancym.wjr@gmail.com>
2023-03-06 19:21:19 +0800

    Flask WTF 表单实例演示

--- app11.py
+++ app11.py
@@ -1,24 +1,32 @@
-from flask import Flask,render_template,request,flash
+from flask import Flask, render_template, request, flash
 from form11 import ContactForm
 
-'''Flask WTF'''
-
+'''
+Flask WTF表单实例演示
+'''
+import os
+secretKey = os.urandom(32)
 app = Flask(__name__)
-app.secret_key='123456'
+app.secret_key = secretKey
 
-@app.route('/',methods=['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def contact():
     form1 = ContactForm()
     if request.method == 'POST':
-        if form1.validate() == False:
+        if not form1.validate():
+            # 做表单检查未通过
             flash('Exist fields are not ready.')
-            print("ddd")
-            return render_template('contact11.html',form=form1)
+            print("Exist fields are not ready")
+            return render_template('contact11.html', form=form1)
+            # 由于此处 form=form1，所以已经填好的值如果不需要修改则不需要重新填写，它不会被删掉
         else:
-            flash("All fileds are ready.")
+            flash("All fields are ready.")
+            print("All fields are ready")
             return render_template('success11.html')
     elif request.method == 'GET':
-        return render_template('contact11.html',form=form1)
+        return render_template('contact11.html', form=form1)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- form11.py
+++ form11.py
@@ -1,12 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField,IntegerField,SubmitField,RadioField,SelectField
+from wtforms import StringField, TextAreaField, IntegerField, SubmitField, RadioField, SelectField
 from wtforms import validators
+# 其中StringField 为 旧版本的 textField
+
 
 class ContactForm(FlaskForm):
-    name = StringField('Name of Student',[validators.InputRequired('Please enter your name.')])
-    Gender = RadioField('Gender',choices=[('M','Male'),('F','Female')])
+    # 自定义contactForm类
+    name = StringField('Name of Student', [validators.InputRequired('Please enter your name.')])
+    # 文本控件：其label为“Name of Student” ，做validators输入有效性检查
+    Gender = RadioField('Gender', choices=[('M', 'Male'), ('F', 'Female')])
+    # 选择性控件：其label为‘Gender’,选项值为M显示为Male 与 值为F显示为Female
     Address = TextAreaField('Address')
-    email = StringField('Email',[validators.InputRequired('Please enter your email address.'),validators.Email('Please enter your email address.')])
+    # 大文本的地址控件
+    email = StringField('Email', [validators.InputRequired('Please enter your email address.'),
+                                  validators.Email('Please enter your email address.')])
+    # 邮件控件，做了两个检查。1、必须输入 2、必须输入有效性格式
     Age = IntegerField('age')
-    language = SelectField('Languages',choices=[('cpp','C++'),('py','Python')])
+    # 整型控件
+    language = SelectField('Languages', choices=[('cpp', 'C++'), ('py', 'Python')])
+    # 下拉控件
     submit = SubmitField('Send')
+    # 提交控件
