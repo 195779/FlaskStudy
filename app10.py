a053196c99ac04d666e8dc58f8a698fd58b90f43
Alan <alancym.wjr@gmail.com>
2023-03-06 18:55:28 +0800

    Flask flash消息闪现演示实例

--- app10.py
+++ app10.py
@@ -2,23 +2,30 @@ from flask import Flask,render_template,request
 from werkzeug.utils import secure_filename
 import os
 
+'''
+Flask 文件上传实例演示
+'''
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploaddir/'
 # 要先在project的目录下创建一个名为uploaddir的文件夹
+
 
 @app.route('/')
 def upload_file():
     return render_template('upload10.html')
 
-@app.route('/uploader',methods=['POST','GET'])
+
+@app.route('/uploader', methods=['POST', 'GET'])
 def uploader():
     if request.method == 'POST':
         f = request.files['file111']
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename)))
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+        # 保存文件到 组成的路径
         # 文件上传到文件夹uploaddir
         return 'file uploaded successfully'
     elif request.method == 'GET':
         return render_template('upload10.html')
+
 
 if __name__ == '__main__':
     Flask.run(debug=True)
